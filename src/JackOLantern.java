737557b8e7e222cad8ac6da9f10607581e5d8a4b
NYC <yibarra0090>
2018-11-26 21:55:27 -0500

    done?

--- src/JackOLantern.java
+++ src/JackOLantern.java
@@ -1,11 +1,35 @@
 public class JackOLantern {
-    public void edit(String replace, int row, int column){
+    public String[][] image;
+    private int width;
+    private int height;
 
+    public JackOLantern (String[][] image){
+        this.image = new String[width][height];
+        this.width = width;
+        this.height = height;
     }
+
+    public void fill(String fillStr){
+        for (int x = 0; x < width; x++){
+            for (int y = 0; y < height; y++){
+                image[x][y] = fillStr;
+            }
+        }
+    }
+
+    public void edit(String str, int row, int column){
+        image[row][column] = str;
+    }
+
     public String toString(){
-
-    }
-    public void fill(String str){
-        
+        String fullImage = "";
+        for (int x = 0; x < width; x++){
+            String newRow = "";
+            for (int y = 0; y < height; y++){
+                newRow += image[x][y];
+            }
+            fullImage += newRow + "\n";
+        }
+        return fullImage;
     }
 }

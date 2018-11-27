ecf301ff80d5de9722c512edb9d527aacdb65c28
BT_1N3_18 <yibarra0090@bths.edu>
2018-11-27 14:45:56 -0500

    Wow~!

--- src/Runner.java
+++ src/Runner.java
@@ -1,23 +1,15 @@
 public class Runner {
     public static void main(String[] args){
-        String [][] face = new String[10][20];
+        String [][] face = new String[10][40];
         JackOLantern face1 = new JackOLantern(face);
-        face1.fill("M");
+        face1.fill("|");
         face1.edit(" ",0,0);
-        face1.edit(" ",4,7);
-        face1.edit(" ",0,7);
-        face1.edit(" ",4,0);
-
-        face1.edit("0",1,2);
-        face1.edit("0",1,5);
-        face1.edit("^",2,3);
-        face1.edit("^",2,4);
-        face1.edit("|",3,1);
-        face1.edit("|",3,2);
-        face1.edit("_",3,3);
-        face1.edit("_",3,4);
-        face1.edit("|",3,5);
-        face1.edit("|",3,6);
+        face1.edit(" ",9,39);
+        face1.edit(" ",0,39);
+        face1.edit(" ",9,0);
+        face1.edit("O",3,6);
+        face1.edit("O",3,33);
+        face1.edit("O",8,20);
         System.out.println(face1);
     }
 }
